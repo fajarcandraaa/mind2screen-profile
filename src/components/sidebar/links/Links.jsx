27c7d906225189285b723e86a5f0427268d3c32f
fajarcandraaa <fajarcandraaa@gmail.com>
2024-02-26 09:12:20 +0700

    feat : Add motion items at Link Sidebar components

--- src/components/sidebar/links/Links.jsx
+++ src/components/sidebar/links/Links.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import { motion } from "framer-motion"
  
 const Links = () => {
 
@@ -12,14 +13,39 @@ const Links = () => {
         "Contact",
     ]
 
+    const variants = {
+        open:{
+            transition:{
+                staggerChildren: 0.1,
+            },
+        },
+        closed:{
+            transition:{
+                staggerChildren: 0.05,
+                staggerDirection: -1,
+            },
+        },
+    }
+
+    const itemVariants = {
+        open:{
+            y: 0,
+            opacity:1,
+        },
+        closed:{
+            y: 50,
+            opacity: 0,
+        },
+    }
+
     return (
-        <div className="links">
+        <motion.div className="links" variants={variants}>
             {items.map(item => (
-            <a href={`#${item}`} key={item}>
+            <motion.a href={`#${item}`} key={item} variants={itemVariants} whileHover={{scale:1.1}} whileTap={{scale: 0.95}}>
                 {item}
-            </a>
+            </motion.a>
             ))}
-        </div>
+        </motion.div>
     )
 }
 
